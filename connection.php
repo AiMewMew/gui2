03d9fa112b9a62e194351cf50bc0d2b17aacfc41
AiMewMew <aipanda007@gmail.com>
2022-02-04 14:24:29 +1100

    Initial commit

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "gui";
+
+
+// Create connection
+$con = mysqli_connect($servername, $username, $password, $dbname);
+?>
